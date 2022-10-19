89c59cdf525c9c74e88ba59dd89ec7de4444b65f
Vansh <vanshayushi@gmail.com>
2022-10-20 00:05:12 +0530

    footer done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import "./Contact.css";
 const Contact = () => {
   return (
-    <div id='contact'>
-      Contact
+    <div id="contact">
+      <h5>Let's connect</h5>
+      <h2>Contact</h2>
+      <div className="container contact_container">
+        <div className="left">
+          <div className="row">
+            <h4>EMAIL</h4>
+            <p className="detail">vanshayushi@gmail.com</p>
+            <p className="send_mail">send a mail</p>
+          </div>
+          <div className="row">
+            <h4>INSTA</h4>
+            <p className="detail">one_shh</p>
+            <p className="send_mail">send a message</p>
+          </div>
+          <div className="row">
+            <h4>WHATSAPP</h4>
+            <p className="detail">8861524553</p>
+            <p className="send_mail">send a message</p>
+          </div>
+        </div>
+        <div className="right">
+          <form action="submit">
+            <input type="text" placeholder="Your Name" name="" id="" />
+            <input type="email" placeholder="Your Email" name="" id="" />
+            <textarea id="" cols="30" placeholder="Your message" rows="9"></textarea>
+            <button className="btn">SUBMIT</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import "./Footer.css";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div id="footer">
+      <h2>VANSH BHARDWAJ</h2>
+      <div className="container footer_container">
+        <div className="footer_nav">
+          <a className="ic" href="#">
+            Home
+          </a>
+          <a className="ic" href="#about">
+            About
+          </a>
+          <a className="ic" href="#experience">
+            Experience
+          </a>
+          <a className="ic" href="#services">
+            Services
+          </a>
+          <a className="ic" href="#contact">
+            Contact Me
+          </a>
+          <a className="ic" href="#testi">
+            Testimonial
+          </a>
+        </div>
+        <div className="social">
+          <AiFillFacebook />
+          <AiFillInstagram />
+          <AiFillLinkedin />
+          <AiFillTwitterSquare />
+        </div>
+        <p>Made with 💙 by VANSH BHARDWAJ</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
